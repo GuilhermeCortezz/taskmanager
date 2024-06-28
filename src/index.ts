0ae7614752163508b9d77b80107d9d63cdd73e00
Cortez <61278332+GuilhermeCortezz@users.noreply.github.com>
2024-06-28 14:39:50 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,6 @@ app.use(bodyParser.json());
 // Rotas para tarefas
 app.use('/tasks', taskRoutes);
 
-// Servir arquivos estáticos do React (opcional, se você precisar servir a aplicação React com o backend)
-// app.use(express.static(path.join(__dirname, '../public')));
-
-// Pega qualquer solicitação que não esteja na API e retorna o index.html do React (opcional)
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public', 'index.html'));
-// });
-
 // Inicia o servidor na porta especificada
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
